67a1b5a92b36129b77af4392ea36e4b902eeea9a
Sharan <sharan.parthasarathy@accenture.com>
2023-08-26 21:30:27 +0530

    Initial Commit "HomePage"

---
+++ Forms_UI/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './modules/home/components/home/home.component';
+import { ProfileComponent } from './modules/Profile/components/profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
